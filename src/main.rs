d60cca1d2fc4764496a12737575fa152e67a6e6b
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-05-25 14:10:21 +0200

    Add simple TensorFlow invocation via python script
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ extern crate schani_image_recognition;
 use schani_image_recognition::recognition::process_image;
 
 #[post("/recognize")]
-fn recognize() -> &'static str {
-    process_image();
-
-    "OK"
+fn recognize() -> String {
+    match process_image() {
+        Ok(out) => out,
+        Err(err) => err.to_string()
+    }
 }
 
 fn main() {
--- src/recognition.rs
+++ src/recognition.rs
@@ -1,8 +1,23 @@
 use std::result::Result;
+use std::process::Command;
 
-pub fn process_image() -> Result<(), &'static str> {
+pub fn process_image() -> Result<String, &'static str> {
     println!("start processing a new image …");
 
+    // TODO: specify script location via .env like
+    // let script = env::var("SCRIPT_LOCATION").expect("SCRIPT_LOCATION must be set");
+
+    let out = match Command::new("python")
+              .arg("tensorflow_models/tutorials/image/imagenet/classify_image.py")
+              .output() {
+        Ok(out) => out,
+        Err(_) => return Err("error running script"),
+    };
+
+    let result = String::from_utf8_lossy(&out.stdout).into_owned();
+
+    println!("{}", result);
+
     println!("finished processing an image.");
-    Ok(())
+    Ok(result)
 }
