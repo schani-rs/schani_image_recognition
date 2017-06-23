825ef56216dc58e5467dfe700230907949589884
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-06-23 17:42:41 +0200

    Return and parse predictions as JSON string
    * Adjust python script to return JSON
    * Use temporary directory to allow parallel classification
    * Return JSON as webservice response
    
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate temporary;
+
 pub mod recognition;
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
 extern crate schani_image_recognition;
 
 use rocket::Data;
-use schani_image_recognition::recognition::process_image;
+use rocket_contrib::JSON;
+use schani_image_recognition::recognition::{Prediction, process_image};
 
 #[post("/recognize", data = "<data>")]
-fn recognize(data: Data) -> String {
-    match process_image(&mut data.open()) {
-        Ok(out) => out,
-        Err(err) => err.to_string(),
-    }
+fn recognize(data: Data) -> Result<JSON<Vec<Prediction>>, String> {
+    Ok(JSON(try!(process_image(&mut data.open()).map_err(
+        |err| err.to_string(),
+    ))))
 }
 
 fn main() {
